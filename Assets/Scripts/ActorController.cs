cce97338a066457d0051480f5f9cbfbd2789222b
Kyle Perri <perri_kyle@ymail.com>
2017-05-16 10:31:05 -0700

    Init Commit
    Added player controller and actor controllers.

---
+++ Assets/Scripts/ActorController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class ActorController : MonoBehaviour {
+
+    public float speed;
+    public float jumpForce;
+    public Transform groundCheck;
+    public LayerMask whatIsGround;
+
+    Rigidbody2D rb;
+    float groundRadius = 0.05f;
+    bool facingRight = true;
+    bool grounded = false;
+    
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+	}
+
+    void FixedUpdate ()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+
+        float move = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(move * speed, rb.velocity.y);
+
+        if (move > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (move < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
+
+    void Update()
+    {
+        if (grounded && Input.GetButtonDown ("Jump"))
+        {
+            rb.AddForce(new Vector2(0, jumpForce));
+        }
+    }
+
+    void Flip ()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    bool isTransferring = false;
+    ActorController[] actors;
+    ActorController currentActor;
+
+	void Start () {
+        actors = GetComponentsInChildren<ActorController>();
+        foreach (ActorController actor in actors)
+        {
+            if (actor.isActiveAndEnabled)
+            {
+                currentActor = actor;
+            }
+        }
+    }
+
+	void FixedUpdate () {
+		if (isTransferring)
+        {
+            if (Input.GetMouseButtonDown (0))
+            {
+                ChooseTarget();
+            }
+        }
+	}
+
+    public void TransferConsciousness ()
+    {
+        isTransferring = true;
+        currentActor.enabled = false;
+    }
+
+    public void CancelTransfer ()
+    {
+        isTransferring = false;
+    }
+
+    void ChooseTarget ()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y), Vector2.zero, 0f);
+
+        if (hit.transform.CompareTag ("PlayableActor"))
+        {
+            ActorController actorController = hit.transform.GetComponent<ActorController>();
+            if (actorController != null)
+            {
+                currentActor.enabled = false;
+                actorController.enabled = true;
+                currentActor = actorController;
+            }
+        }
+    }
+}
